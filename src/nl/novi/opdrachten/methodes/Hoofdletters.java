cafe4388d781fbb46b93acdfcd368d7e9cca0442
Cyan Dalebout <cyan@wervelwindmedia.nl>
2023-02-25 20:34:52 +0100

    Hoofdletters

--- src/nl/novi/opdrachten/methodes/Hoofdletters.java
+++ src/nl/novi/opdrachten/methodes/Hoofdletters.java
@@ -18,18 +18,29 @@ import java.util.List;
 public class Hoofdletters {
 
     public static void main(String[] args) {
-        List<String> curstomerNames = new ArrayList<>();
+        List<String> customerNames = new ArrayList<>();
 
-        curstomerNames.add("nick piraat");
-        curstomerNames.add("michael jackson");
-        curstomerNames.add("glennis grace");
-        curstomerNames.add("dreetje hazes");
-        curstomerNames.add("robbie williams");
-        curstomerNames.add("michiel de ruyter");
-        curstomerNames.add("sjaak polak");
-        curstomerNames.add("jan van jansen");
-        curstomerNames.add("henk den hartog");
-        curstomerNames.add("mo el-mecky");
-        curstomerNames.add("fredje kadetje");
+        customerNames.add("nick piraat");
+        customerNames.add("michael jackson");
+        customerNames.add("glennis grace");
+        customerNames.add("dreetje hazes");
+        customerNames.add("robbie williams");
+        customerNames.add("michiel de ruyter");
+        customerNames.add("sjaak polak");
+        customerNames.add("jan van jansen");
+        customerNames.add("henk den hartog");
+        customerNames.add("mo el-mecky");
+        customerNames.add("fredje kadetje");
+
+        //Takes a stream of list
+        System.out.println("Normal");
+        customerNames.stream().map(customers -> customers.toUpperCase())
+                .forEach(customers -> System.out.println(customers));
+
+        //Lambda replacement
+        System.out.println("Lambda");
+        customerNames.stream().map(String::toUpperCase)
+                .forEach(System.out::println);
+
     }
 }
